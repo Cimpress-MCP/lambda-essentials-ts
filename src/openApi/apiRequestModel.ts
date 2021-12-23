60f1d4aca113478a8b2a6c4d7fbbc9eb0349768f
mmaruniak <mmaruniak@cimpress.com>
2021-12-23 15:31:54 +0100

    Release 4.1.4

--- src/openApi/apiRequestModel.ts
+++ src/openApi/apiRequestModel.ts
@@ -13,7 +13,7 @@ export interface ApiRequest<Body = any, Query = any> {
   headers: Record<string, string>;
   pathParameters: Record<string, string>;
   queryStringParameters?: Query;
-  multiValueQueryStringParameters?: Query;
+  multiValueQueryStringParameters?: any;
 }
 
 export interface PostRequest<Body = any, Query = any> extends ApiRequest<Body, Query> {
